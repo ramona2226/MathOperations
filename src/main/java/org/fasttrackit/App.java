32db9b7d34ddd7c47ea0387a1b36b77711f40017
Ramona2226 <brisc.ramona22@yahoo.com>
2020-06-16 08:43:22 +0300

    Methods with different return values added.

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -1,13 +1,25 @@
 package org.fasttrackit;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        Methods methodsclass = new Methods();
+        //numere intregi int
+        System.out.println(methodsclass.sum(10 ,5));
+
+        System.out.println(methodsclass.difencence(10,5));
+
+        System.out.println(methodsclass.multiplication(10, 5));
+
+        System.out.println(methodsclass.division(10,5));
+
+// numere zecimale double
+        System.out.println(methodsclass.sum2(7.5, 2.3));
+
+        System.out.println(methodsclass.difencence2(7.5, 4.1));
+
+        System.out.println(methodsclass.multiplication2(2.3, 3.3));
+
+        System.out.println(methodsclass.division2(8.4, 2.2));
     }
 }
--- src/main/java/org/fasttrackit/Methods.java
+++ src/main/java/org/fasttrackit/Methods.java
@@ -1,4 +1,37 @@
 package org.fasttrackit;
 
 public class Methods {
+
+    public int sum(int number1, int number2) {
+        return number1 + number2;
+    }
+
+    public int difencence(int number1, int number2) {
+        return number1 - number2;
+    }
+
+
+    public int multiplication(int number1, int number2) {
+        return number1 * number2;
+    }
+
+    public int division(int number1, int number2) {
+        return number1 / number2;
+    }
+
+    public double sum2(double number1, double number2) {
+        return number1 + number2;
+    }
+
+    public double difencence2(double number1, double number2) {
+        return number1 - number2;
+    }
+
+
+    public double multiplication2(double number1, double number2) {
+        return number1 * number2;
+    }
+    public double division2(double number1, double number2) {
+        return number1 / number2;
+}
 }
