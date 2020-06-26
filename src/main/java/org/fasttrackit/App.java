60370121ab727ef4b5cb852a6e572cda9a04961c
Ramona2226 <brisc.ramona22@yahoo.com>
2020-06-26 22:40:35 +0300

    Supraincarcarea metodelor.

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -4,24 +4,24 @@ public class App {
     public static void main(String[] args) {
         Methods methods = new Methods();
 
-int number1 = 10;
-int number2 = 5;
- double number1d = 7.5;
- double number2d = 3.4;
- }
+        int number1 = 10;
+        int number2 = 5;
+        double number1d = 7.5;
+        double number2d = 3.4;
 
-  //numere intregi int
-        System.out.println(methods.sum(10, 5))
+
+        //numere intregi int
+        System.out.println(methods.sum(number1, number2));
         System.out.println(methods.sum(7.5, 3.4));
 
 
-        System.out.println(methods.difencence(10, 5));
+        System.out.println(methods.difencence(number1, number2));
         System.out.println(methods.difencence(7.5, 3.4));
 
-        System.out.println(methods.multiplication(10, 5));
+        System.out.println(methods.multiplication(number1, number2));
         System.out.println(methods.multiplication(7.5, 3.4));
 
-        System.out.println(methods.division(10, 5));
+        System.out.println(methods.division(number1, number2));
         System.out.println(methods.division(7.5, 3.4));
 
 
@@ -35,4 +35,6 @@ int number2 = 5;
 //        System.out.println(methodsclass.division2(8.4, 2.2));
 
     }
+}
+
 
--- src/main/java/org/fasttrackit/Methods.java
+++ src/main/java/org/fasttrackit/Methods.java
@@ -7,15 +7,15 @@ public class Methods {
         return number1 + number2;
     }
     public double sum(double number1d, double number2d){
-        return sum(number1d, number2d);
+        return number1d + number2d;
 
     }
 
     public double difencence(int number1, int number2) {
-        return difencence(number1, number2);
+        return number1 - number2;
     }
-public double difencence(double number1d, double number2d) {
-        return difencence(number1d, number2d);
+    public double difencence(double number1d, double number2d) {
+        return number1d - number2d;
 
 }
 
@@ -24,7 +24,7 @@ public double difencence(double number1d, double number2d) {
     }
 
     public double multiplication(double number1d, double number2d) {
-        return multiplication(number1d, number2d);
+        return number1d * number2d;
     }
 
     public int division(int number1, int number2) {
@@ -32,7 +32,7 @@ public double difencence(double number1d, double number2d) {
     }
 
     public double division(double number1d, double number2d){
-        return division(number1d, number2d);
+        return number1d % number2d;
     }
 //
 //    public double sum2(double number1, double number2) {
